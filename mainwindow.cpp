02c559c436119ea3f5f24ce7f57f02db6850df33
Bogdan__ <bogerbloger@yandex.ru>
2025-01-14 14:34:53 +0300

    norm_graph

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -8,19 +8,16 @@
 #include <QCheckBox>
 #include <QVector>
 #include <QMap>
-#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
-    // Инициализация QCustomPlot
     customPlot = new QCustomPlot(this);
     ui->plotArea->setLayout(new QVBoxLayout);
     ui->plotArea->layout()->addWidget(customPlot);
 
-    // Связываем пункт меню "Открыть" с обработчиком
     connect(ui->actionOpenFile, &QAction::triggered, this, &MainWindow::openFile);
 }
 
@@ -31,18 +28,15 @@ MainWindow::~MainWindow()
 
 void MainWindow::openFile()
 {
-    // Открываем диалог выбора файла
     QString filePath = QFileDialog::getOpenFileName(this, tr("Открыть файл"), "", tr("TAB Files (*.tab);;All Files (*)"));
     if (filePath.isEmpty())
         return;
 
-    // Запрашиваем количество строк для парсинга
     bool ok;
     int lineCount = QInputDialog::getInt(this, tr("Количество строк"), tr("Сколько строк парсить?"), 10, 1, 1000000, 1, &ok);
     if (!ok)
         return;
 
-    // Открываем файл
     QFile file(filePath);
     if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
     {
@@ -52,84 +46,43 @@ void MainWindow::openFile()
 
     QTextStream in(&file);
     QStringList variableNames;
-    QVector<double> timeData; // Глобальное время
+    QVector<double> timeData;
     QMap<QString, QVector<double>> variablesData;
 
-    // Чтение заголовков
     if (!in.atEnd())
     {
         QString headerLine = in.readLine();
         variableNames = headerLine.split('\t', Qt::SkipEmptyParts);
-
-        qDebug() << "Header line:" << headerLine;
-        qDebug() << "Variable names:" << variableNames;
-
-        if (!variableNames.isEmpty() && variableNames.first() == "time") {
-            variableNames.removeFirst(); // Убираем "time", если он присутствует
+        if (!variableNames.isEmpty() && variableNames.first() == "time")
+        {
+            variableNames.removeFirst();
         }
     }
 
     int parsedLines = 0;
-    double globalTime = 0.0;
-
-    // Чтение строк файла
+    double currentTime = 0.0; 
     while (!in.atEnd() && parsedLines < lineCount)
     {
         QString line = in.readLine();
         QStringList values = line.split('\t', Qt::SkipEmptyParts);
-
-        qDebug() << "Parsed line:" << line;
-        qDebug() << "Split values:" << values;
-
-        if (values.size() != variableNames.size() + 1) {
-            qDebug() << "Skipping line due to mismatch between header and row sizes.";
+        if (values.size() < variableNames.size() + 1)
             continue;
+
+        
+        timeData.append(currentTime);  
+
+       
+        currentTime += 0.0025;
+
+        for (int i = 0; i < variableNames.size(); ++i)
+        {
+            variablesData[variableNames[i]].append(values[i + 1].toDouble());
         }
-
-        bool ok = false;
-        double dtValue = values[0].toDouble(&ok);
-        if (!ok) {
-            qDebug() << "Skipping line due to invalid dt value:" << values[0];
-            continue;
-        }
-
-        // Проверяем, что dtValue действительно 0.0025
-        if (qFabs(dtValue - 0.0025) > 1e-6) {
-            qDebug() << "Unexpected dtValue:" << dtValue;
-        }
-
-        globalTime += dtValue;
-        timeData.append(globalTime);
-
-        for (int i = 0; i < variableNames.size(); ++i) {
-            bool valueOk = false;
-            double varValue = values[i + 1].toDouble(&valueOk);
-            if (valueOk) {
-                variablesData[variableNames[i]].append(varValue);
-            } else {
-                qDebug() << "Invalid value for variable:" << variableNames[i] << ", at line:" << parsedLines;
-            }
-        }
-
         parsedLines++;
-    }
-
-
-    qDebug() << "Final timeData:" << timeData;
-    for (const QString &key : variablesData.keys()) {
-        qDebug() << "Variable:" << key << ", Data:" << variablesData[key];
     }
 
     file.close();
 
-    // Проверка данных
-    qDebug() << "timeData:" << timeData;
-    for (const QString &key : variablesData.keys())
-    {
-        qDebug() << "Variable:" << key << ", Data:" << variablesData[key];
-    }
-
-    // Обновление интерфейса
     updateVariables(variableNames);
 
     this->timeData = timeData;
@@ -138,9 +91,10 @@ void MainWindow::openFile()
     redrawGraph();
 }
 
+
 void MainWindow::updateVariables(const QStringList &variables)
 {
-    // Очистка списка переменных
+
     QLayout *layout = ui->variablesLayout;
     QLayoutItem *item;
     while ((item = layout->takeAt(0)) != nullptr)
@@ -149,7 +103,6 @@ void MainWindow::updateVariables(const QStringList &variables)
         delete item;
     }
 
-    // Добавление чекбоксов
     for (const QString &var : variables)
     {
         QCheckBox *checkBox = new QCheckBox(var, this);
@@ -164,11 +117,6 @@ void MainWindow::toggleVariableOnGraph(const QString &variable, bool visible)
 {
     if (visible)
     {
-        if (timeData.size() != variablesData[variable].size())
-        {
-            qDebug() << "Размеры timeData и" << variable << "не совпадают!";
-            return;
-        }
 
         QCPGraph *graph = customPlot->addGraph();
         graph->setData(timeData, variablesData[variable]);
@@ -176,6 +124,7 @@ void MainWindow::toggleVariableOnGraph(const QString &variable, bool visible)
     }
     else
     {
+
         for (int i = 0; i < customPlot->graphCount(); ++i)
         {
             if (customPlot->graph(i)->name() == variable)
@@ -194,6 +143,6 @@ void MainWindow::redrawGraph()
     customPlot->xAxis->setLabel("Время");
     customPlot->yAxis->setLabel("Значение");
     customPlot->legend->setVisible(true);
-    customPlot->rescaleAxes(); // Автоматическое масштабирование осей
-    customPlot->replot();      // Отрисовка графиков
+    customPlot->rescaleAxes();
+    customPlot->replot();
 }
